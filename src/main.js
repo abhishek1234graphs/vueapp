040d825631760f6816df8d2bc4a02dacd29b51b6
Abhishek Sharma <abhishek_graphs@hotmail.com>
2021-12-19 14:08:53 +0530

    Added header component. Changed vue version

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router.js';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import routes from './router';
 
-const app = createApp(App).mount('#app');
-app.use(router);
-app.mount('#app');
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+const app = new Vue({
+    router,
+    template: '<App />',
+    components: {
+        App
+    }
+}).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,24 +1,23 @@
-import { createRouter,createWebHistory } from 'vue-router';
+import VueRouter ,{createWebHistory } from 'vue-router';
 
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachDetailRegistration from './pages/coaches/CoachDetailRegistration.vue';
-import ContactCoach from './pages/requests/CoachDetail.vue';
-import RequestsReceived from './pages/requests/CoachDetail.vue';
+import CoachDetailRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
-const router = createRouter({
-    history:createWebHistory(),
-    routes:[
-        { path:'/',redirect:'/coaches'},
-        { path: '/coaches',component:CoachesList},
+const routes = [
         { path: '/coaches/:id',component:CoachDetail,children:[
             {path:'contact',component:ContactCoach} // /contact/c1/contact
         ]},
+        { path: '/coaches',component:CoachesList},
+        
         { path: '/register',component:CoachDetailRegistration},
         { path: '/requests',component:RequestsReceived},
-        { path: '/:notFound(.*)',component:NotFound}
-    ]
-});
+        { path: '/:notFound(.*)',component:NotFound},
+        { path:'/',redirect:'/coaches'},
+    
+];
 
-export default router;
+export default routes;
